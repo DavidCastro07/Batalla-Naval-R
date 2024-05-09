38df585f112290f7b0ecc0b756a39598c761c4fb
Ramiro Tabolini <ramirotabolini@gmail.com>
2024-05-08 22:16:44 -0300

    Agregue JUnit 5 a las dependencias, tambieen creer un modulo llamado Test donde implmentamos los test

---
+++ test/src/MyTestClass.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class MyTestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
