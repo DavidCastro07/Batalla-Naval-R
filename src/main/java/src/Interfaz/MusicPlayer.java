6c02ed4fe9d099fe0773dc70dfc684ef0e367aec
RoblesYesica <karen.yesica.robles>
2024-06-15 16:24:54 -0300

    add class MusicPlayer

--- src/main/java/src/Interfaz/MusicPlayer.java
+++ src/main/java/src/Interfaz/MusicPlayer.java
@@ -1,2 +1,59 @@
-package src.Interfaz;public class MusicPlayer {
+package src.Interfaz;
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
+public class MusicPlayer {
+    private Clip audioClip;
+    private AudioInputStream audioStream;
+
+    public MusicPlayer(String path){
+        //create an AudioInputStream from a given sound file
+        File audioFile = new File(path);
+        try {
+            audioStream = AudioSystem.getAudioInputStream(audioFile);
+        }
+        catch (UnsupportedAudioFileException | IOException e) {
+            e.printStackTrace();
+        }
+
+        //acquire audio format and create a DataLine.Info object
+        AudioFormat format = audioStream.getFormat();
+        var info = new DataLine.Info(Clip.class, format);
+
+        //obtain the Clip
+        try {
+            audioClip = AudioSystem.getClip();
+            audioClip.open(audioStream);
+        }
+        catch (LineUnavailableException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void play() {
+        new Thread( () -> {
+            audioClip.setFramePosition(0);
+            audioClip.start();
+        }){}.start();
+    }
+
+    public void loop() {
+        new Thread( () -> {
+            audioClip.setFramePosition(0);
+            audioClip.loop(Clip.LOOP_CONTINUOUSLY);
+        }){}.start();
+    }
+
+    public void stop() {
+        audioClip.stop();
+    }
+
+
+    public void close() {
+        if (audioClip != null) {
+            audioClip.close();
+        }
+    }
 }
