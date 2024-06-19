e96868ccab27d851309aca8283eb0a1ad5e876ae
david <dgcastro21@gmail.com>
2024-06-18 23:05:45 -0300

    resolviendo porcentaje

--- src/main/java/src/Clases/Porcentajes.java
+++ src/main/java/src/Clases/Porcentajes.java
@@ -6,20 +6,24 @@ import src.Clases.Observer;
 
 
     public class Porcentajes implements Observer{
-        private int porcentaje;
-
+        private int porcentaje=0;
+        private int hundidos;
+        private int dispaross;
         private Juego juego;
 
         public void update(int disparos, int Hundidos, int Perdidos) {
-            ImprimirPorcentaje( disparos,Hundidos);
+            hundidos=Hundidos;
+            dispaross=disparos;
+            ImprimirPorcentaje(dispaross,hundidos);
         }
         public Porcentajes(Juego juego) {
             this.juego = juego;
         }
 
         private void ImprimirPorcentaje(int disparo, int hundido) {
-            float tasa = (float) hundido /disparo;
-            porcentaje= (int) tasa;
+            int undido=hundido*3;
+            float tasa = (float)undido /disparo;
+            porcentaje= (int) (tasa*100);
             this.juego.jExito.setText(String.valueOf(porcentaje));
         }
         }
